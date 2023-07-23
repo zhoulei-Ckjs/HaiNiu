d59a2034a5bf383350895daceb68879b658241e8
zhoulei <56968749@qq.com>
2023-07-23 11:03:14 +0800

    [引导扇区setup]-进入内核::增加读取磁盘函数read_disk

--- oskernel/boot/setup.asm
+++ oskernel/boot/setup.asm
@@ -78,6 +78,82 @@ protect_mode:
 
     jmp $                       ; 停在这里
 
+; 读取磁盘代码（lba方式读盘），供32位保护模式调用，实模式下不可调用（因为放在了bits 32下了）
+; 调用方式：
+; mov ecx, XXX      ; 从哪个扇区开始读，lba扇区下标从0开始
+; mov bl, XXX       ; 读取扇区数量
+; mov edi, XXX      ; 读取的数据要写入到哪里
+read_disk:
+    ; 0x1f2 8bit 指定读取或写入的扇区数
+    mov dx, 0x1f2
+    mov al, bl      ; 扇区数
+    out dx, al      ; 向0x1f2发送命令
+
+    ; 0x1f3 8bit lba地址的第八位 0-7
+    inc dx
+    mov al, cl      ; 从哪个扇区开始读，LBA low
+    out dx, al
+
+    ; 0x1f4 8bit lba地址的中八位 8-15
+    inc dx
+    shr ecx, 8
+    mov al, cl      ; LBA中8位
+    out dx, al
+
+    ; 0x1f5 8bit iba地址的高八位 16-23
+    inc dx
+    shr ecx, 8      ; 右移8位
+    mov al, cl
+    out dx, al      ; 发送LBA高8位
+
+    ; 0x1f6 8bit
+    ; 0-3 位iba地址的24-27
+    ; 4 0表示主盘 1表示从盘
+    ; 5、7位固定为1
+    ; 6 0表示CHS模式，1表示LAB模式
+    inc dx
+    shr ecx, 8
+    and cl, 0b1111      ; 将高四位置为 0
+    mov al, 0b1110_0000 ;
+    or al, cl
+    out dx, al; 主盘 - LBA 模式
+
+    ; 0x1f7 8bit  命令或状态端口
+    inc dx
+    mov al, 0x20    ; 发送读盘命令
+    out dx, al
+
+    ; 清空循环次数
+    xor ecx, ecx
+    mov cl, bl          ; 得到读写扇区的数量
+.read_sector:
+    push cx             ; 保存 cx
+    call .read_check    ; 读取一个扇区
+    pop cx              ; 恢复 cx
+    loop .read_sector
+    ret
+
+    ; 检测硬盘状态
+    ; 3 0表示硬盘未准备好与主机交换数据 1表示准备好了
+    ; 7 0表示硬盘不忙 1表示硬盘忙
+    ; 0 0表示前一条指令正常执行 1表示执行出错 出错信息通过0x1f1端口获得
+.read_check:
+    mov dx, 0x1f7
+    in al, dx
+    and al, 0b10001000  ; 取硬盘状态的第3、7位
+    cmp al, 0b00001000  ; 硬盘数据准备好了且不忙了
+    jnz .read_check     ; 否则就一直检查下去
+
+    ; 从0x1f0 读数据
+    mov dx, 0x1f0
+    mov cx, 256         ; 要循环256次，每次读 2 字节
+.read_data:
+    in ax, dx           ; 读取 2 字节
+    mov [edi], ax       ; 放到edi的位置
+    add edi, 2
+    loop .read_data
+    ret
+
 ; 注意这里要加一个bit 16，因为没进入保护模式前调用32位编码的函数会报错 eip > cs.limit
 [BITS 16]
 ; 如何调用
