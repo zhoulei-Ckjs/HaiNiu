276e103d18e9a07167bc714dfca53e9c8930efed
zhoulei <56968749@qq.com>
2023-08-09 06:33:09 +0800

    [内核]-console控制台::初始化::清屏

--- oskernel/include/linux/tty.h
+++ oskernel/include/linux/tty.h
@@ -1,6 +1,9 @@
 #ifndef HAINIU_TTY_H
 #define HAINIU_TTY_H
 
+/**
+ * 初始化控制台
+ */
 void console_init(void);
 
 #endif //HAINIU_TTY_H
--- oskernel/kernel/chr_drv/console.c
+++ oskernel/kernel/chr_drv/console.c
@@ -1,9 +1,11 @@
 #include "../../include/linux/tty.h"
 
-void console_init(void)
+#define MEM_BASE 0xB8000                            // 显卡内存起始位置
+
+void console_clear()
 {
     char *p = "[init::main.c] : in kernel_main ...";
-    char* video = (char*)0xb8000 + 80 * 2 * 2;       //显示在第三行，0xb8000是显存和内存的映射位置，每行80字符，每个字符2字节
+    char* video = (char*)MEM_BASE + 80 * 2 * 2;       // 显示在第三行，0xb8000是显存和内存的映射位置，每行80字符，每个字符2字节
     while(*p != '\0')
     {
         *video = *p;
@@ -12,4 +14,9 @@ void console_init(void)
         ++video;
         p++;
     }
+}
+
+void console_init(void)
+{
+    console_clear();
 }
