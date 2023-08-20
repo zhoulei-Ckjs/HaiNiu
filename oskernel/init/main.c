568dd80f657e779574c057afd5baaffed291ddb6
zhoulei <56968749@qq.com>
2023-08-21 06:48:54 +0800

    [内核]-IO::console_write()::处理换行符 '\n'，即0x0A

--- oskernel/init/main.c
+++ oskernel/init/main.c
@@ -5,6 +5,14 @@ void kernel_main(void)
 {
     console_init();
 
+    /* 测试 console_write */
+    // 测试字符串输出
+    char *p = "HaiNiu";
+    console_write(p, 6);
+    // 测试换行
+    p = "HaiNiu\n";                     // '\n'为转义字符，在内存中占一个字节，即 0A
+    console_write(p, 7);
+
     printk("char is : %c", 'H');                // 打印一个字符
 
     while (true);
