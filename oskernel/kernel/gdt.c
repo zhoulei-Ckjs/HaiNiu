a56fcc8473db3e2f53d26b79043bec0c26f8ff33
zhoulei <56968749@qq.com>
2023-10-14 17:10:47 +0800

    [内核]-GDT::增加段描述符，并测试准确

--- oskernel/kernel/gdt.c
+++ oskernel/kernel/gdt.c
@@ -19,4 +19,11 @@ void gdt_init()
     printk("init gdt...\n");
     __asm__ volatile ("sgdt gdt_ptr;");             // 加载 gdt 表信息，保存到 gdt_ptr 中
     memcpy(&gdt, gdt_ptr.base, gdt_ptr.limit);      // 拷贝 gdt 中段描述符出来
+    gdt_item_t *item = &gdt[1];
+    printk("%x\n", item->limit_low);            // 应该是 0xffff
+    printk("%x\n", item->base_low);            // 应该是 0x0000
+    printk("%x\n", item->base_low);            // 应该是 0x0000
+    printk("%x\n", item->segment);              // 应该是 0x1
+    printk("%x\n", item->DPL);                  // 应该是 0x0
+    printk("%x\n", item->present);              // 应该是 0x1
 }
